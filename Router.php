4dbf1dcd385eea6f3857c4059d97ad96414e142a
ZunoRick <zunosan.ricardo506@gmail.com>
2021-05-28 02:00:25 -0500

    Agregando las URL y controlador de propiedad

--- Router.php
+++ Router.php
@@ -3,5 +3,26 @@
 namespace MVC;
 
 class Router{
-    
+    public $rutasGET = [];
+    public $rutasPOST = [];
+
+    public function get($url, $fn){
+        $this->rutasGET[$url] = $fn;
+    }
+
+    public function comprobarRutas(){
+        $urlActual = $_SERVER['PATH_INFO'] ?? '/';
+        $metodo = $_SERVER['REQUEST_METHOD'];
+        
+        if ($metodo === 'GET'){
+            $fn = $this->rutasGET[$urlActual] ?? null;
+        }
+
+        if ($fn) {
+            //La URL exister y hay una función asociada
+            call_user_func($fn, $this);
+        } else{
+            echo "Error 404";
+        }
+    }
 }
