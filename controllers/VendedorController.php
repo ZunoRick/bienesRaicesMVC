d09889620d8c5cb2a002f26b2840908f0ffa7ff5
ZunoRick <zunosan.ricardo506@gmail.com>
2021-05-30 17:23:19 -0500

    Agregando eliminar vendedor en el panel de admin

--- controllers/VendedorController.php
+++ controllers/VendedorController.php
@@ -27,11 +27,42 @@ class VendedorController{
         ]);
     }
 
-    public static function actualizar(){
-        echo "Actualizando Vendedores";
+    public static function actualizar(Router $router){
+        $id = validarORedireccionar('/public/admin');
+        $vendedor = Vendedor::find($id);
+        $errores = Vendedor::getErrores();
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST'){
+            //Asignar los atributos
+            $args = $_POST['vendedor'];
+            $vendedor->sincronizar($args);
+    
+            $errores = $vendedor->validar();
+    
+            if (empty($errores)) {
+                $vendedor->guardar();
+            }
+        }
+
+        $router->render('vendedores/actualizar', [
+            'vendedor' => $vendedor,
+            'errores' => $errores
+        ]);
     }
 
     public static function eliminar(){
-        echo "Eliminando Vendedores";
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $id = $_POST['id'];
+            $id = filter_var($id, FILTER_VALIDATE_INT);
+    
+            //Validar ID
+            if ($id) {
+                $tipo = $_POST['tipo'];
+                if (validarTipoContenido($tipo)) {
+                    $vendedor = Vendedor::find($id);
+                    $vendedor->eliminar();
+                }
+            }
+        }
     }
 }
